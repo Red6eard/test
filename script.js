ec6f5a683dc0b0eccddf8276d7c138c0e2551610
Red6eard <vadim.71rus@gmail.com>
2023-09-26 13:54:02 +0300

    добавлен первый тест

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-document.write('Hello, world');
-console.log('Hello, world');
+
+
+const newPerson = {
+  id: 00000,
+  fullName: {
+    firstName: "firstName",
+    surname: "surname",
+    lastName: "lastName",
+  },
+  gender: "gender",
+  age: 0,
+  address: {
+    country: "country",
+    city: "city",
+    street: "street",
+    house: "house",
+    apartment: 0
+  },
+  married: true
+};
+
+console.log(newPerson);
+
+
+const updatePerson = (obj) => {
+  const pers1 = JSON.parse(JSON.stringify(obj));
+  pers1.fullName.firstName = "Vadim";
+  return pers1
+};
+
+console.log(updatePerson(newPerson));
